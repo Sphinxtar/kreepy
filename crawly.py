98d49eb4130c7e3ac311ff9718d8ea095331f992
“Spinxtar” <sphinxtar@gmail.com>
2021-04-23 04:59:32 -0700

    modified:   crawly.py

--- crawly.py
+++ crawly.py
@@ -5,15 +5,21 @@ from bs4 import BeautifulSoup
 homepage = "https://glhtracker.automationtrainer.com"
 hrefs = []
 
-def probe(site):
-    print("probing: " + site)
+def probe(site,top):
+    if (top):
+        print("<site>")
+    print("<link>")
+    print("<url>" + site + "</url>")
     apage = requests.get(site) 
-    print("returned: ", apage.status_code)
+    print("<status>"+str(apage.status_code)+"</status>")
+    print("</link>")
     soup = BeautifulSoup(apage.content, features="lxml")
     links = soup.find_all("a")
     for link in links:
         if link.get("href") not in hrefs and link.get("href") != homepage:
             hrefs.append(link.get("href"))
-            probe(link.get("href"))
+            probe(link.get("href"),0)
+    if (top):
+        print("</site>")
 
-probe(homepage)
+probe(homepage,1)
